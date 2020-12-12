a50dc6653c07d7b5a9a2ee6cc9c3d2425e61a689
WrathofBhuvan11 <36232037+WrathofBhuvan11@users.noreply.github.com>
2020-12-12 23:58:47 +0530

    Update bitvec.py

--- bitvec.py
+++ bitvec.py
@@ -34,130 +34,216 @@ class BitVec:
             else:
                 bv.val = int('0b'+bs[stop:start+1:step], base=2)
             return bv
-    
+        
+    # represent method
     def __repr__(self):
         return "0b"+str(bin(self.val))[2:].zfill(self.size) #return bin(self.val)
-
+    
+    #add method
     def __add__(self, lhs):
         size = max([self.size, lhs.size])
-        val = (self.val + lhs.val) & ((1<<size) - 1)
+        val = (self.val + lhs.val)
+        return BitVec(size, val)
+    
+    # inplace add method
+    def __iadd__(self, lhs):
+        size = self.size
+        if(type(lhs) is int):
+            val = (self.val + lhs) & ((1<<size) - 1)
+        else:
+            val = (self.val + lhs.val) & ((1<<size) - 1)
         return BitVec(size, val)
 
+    #sub method
     def __sub__(self, lhs):
         size = max([self.size, lhs.size])
         val = (self.val + ~lhs.val+1)
         return BitVec(size, val)
 
-    def __mul__(self, lhs):
-        size = self.size + lhs.size
-        val = (self.val * lhs.val)
+    #inplace sub method
+    def __isub__(self, lhs):
+        size = self.size
+        if(type(lhs) is int):
+            val = (self.val + ~lhs+1) 
+        else:
+            val = (self.val + ~lhs.val+1) 
         return BitVec(size, val)
-        
+    
+    #mul method
+    def __mul__(self, lhs):
+        val = (self.val * lhs.val)
+        size = val.bit_length()
+        return BitVec(size, val)
+           
+    #inplace mul method        
+    def __imul__(self, lhs):
+        size = self.size
+        if(type(lhs) is int):
+            val = self.val * lhs & ((1<<size) - 1)
+        else:
+            val = self.val * lhs.val & ((1<<size) - 1)
+        return BitVec(size, val)
+    
+    #power method
     def __pow__(self, lhs):
         val = (self.val ** lhs.val)
-        size = len(bin(val)[2:]) ## should this be used to optimze size for power, mul etc ? given its string operation, can save the size
+        size = val.bit_length()
         return BitVec(size, val)
 
+    #div method
     def __truediv__ (self, lhs):
-        size = max([self.size, lhs.size])
-        num = self.val 
-        counter = 0
-        while(num>=lhs.val):
-            num -= lhs.val 
-            counter+=1
-        return BitVec(size, val=counter)
-    
-    def fast_div(self, lhs):
         size = max([self.size, lhs.size])
         val = self.val // lhs.val
         return BitVec(size, val)
-        
-    def __mod__ (self, lhs):
-        size = max([self.size, lhs.size])
-        num = self.val 
-        counter = 0
-        while(num>=lhs.val):
-            num -= lhs.val 
-            counter+=1
-        return BitVec(size, val=num)
 
-    def fast_mod(self, lhs):
+    #inplace div method
+    def __itruediv__ (self, lhs):
+        size = self.size
+        if(type(lhs) is int):
+            val = self.val // lhs
+        else:
+            val = self.val // lhs.val
+        return BitVec(size, val)
+
+    #modulo method
+    def __mod__ (self, lhs):
         size = max([self.size, lhs.size])
         val = self.val % lhs.val
         return BitVec(size, val)
     
+    #inplace modulo method
+    def __imod__ (self, lhs):
+        size = self.size
+        if(type(lhs) is int):
+            val = self.val % lhs
+        else:
+            val = self.val % lhs.val
+        return BitVec(size, val)
+
+    #left shift method
     def __lshift__(self, lhs):
         size = self.size
-        val = (self.val << lhs.val)
+        if(type(lhs) is int):
+            val = self.val << lhs
+        else:
+            val = (self.val << lhs.val)
         return BitVec(size, val)
         
+    # right shift method        
     def __rshift__(self, lhs):
         size = self.size
-        val = (self.val >> lhs.val)
+        if(type(lhs) is int):
+            val = self.val >> lhs
+        else:
+            val = (self.val >> lhs.val)
         return BitVec(size, val)
-
+        
+    # circular left shift method
     def clshift(self, lhs):
-        size = self.size 
-        val = (self.val << lhs.val)|(self.val >> (size - lhs.val)) 
+        size = self.size
+        if(type(lhs) is int):
+            val = (self.val << lhs%size)|(self.val >> (size - lhs%size)) 
+        else:
+            val = (self.val << lhs.val%size)|(self.val >> (size - lhs.val%size)) 
         return BitVec(size, val)
-
+    
+    #circular right shift method
     def crshift(self, lhs):
         size = self.size
-        val  = (self.val >> lhs.d) | (self.val << (size - lhs.val)) & ((2**size)-1)
-        return BitVec(size, val) 
-    
+        if(type(lhs) is int):
+            val = (self.val >> lhs%size) | (self.val << (size - lhs%size)) 
+        else:
+            val = (self.val >> lhs.val%size) | (self.val << (size - lhs.val%size)) 
+        return BitVec(size, val)
+
+    # bitwise and method
     def __and__(self, lhs):
         size = max([self.size, lhs.size])
         val = self.val & lhs.val
         return BitVec(size, val)
     
+    #bitwise or method
     def __or__(self, lhs):
         size = max([self.size, lhs.size])
         val = self.val | lhs.val 
         return BitVec(size,val)
-        
+    
+    #bitwise XOR method
     def __xor__(self, lhs):
         size = max([self.size, lhs.size])
         val = self.val ^ lhs.val 
         return BitVec(size,val)
-        
+    
+    # invert method
     def __invert__(self):
         size = self.size
         val = ~self.val  
         return BitVec(size,val)
-
+    
+    #to_hex convert method
     def hex(self):
         return hex(self.val)
 
+    #to_dec convert method
     def dec(self):
         return str(self.val)
 
+    #to_bin convert method
     def bin(self):
         return bin(self.val())
 
+    #equal_to method
     def __eq__(self, lhs):
         bool_val = (self.val == lhs.val)
         return bool_val ## BitVec(size=1, val = int(bool_val))
     
+    #less than method
     def __lt__(self, lhs):
         bool_var = self.val < lhs.val 
-        return bool_var ## BitVec(size=1, val = int(bool_val))
+        return bool_var ## BitVec(size=1, val = int(bool_valo))
     
+    #less than or equal to method
     def __le__(self, lhs):
         bool_var = self.val <= lhs.val 
         return bool_var ## BitVec(size=1, val = int(bool_val))
     
+    # not equal to method
     def __ne__(self, lhs):
         bool_var = self.val != lhs.val 
         return bool_var ## BitVec(size=1, val = int(bool_val))
 
+    # greater than method
     def __gt__(self, lhs):
         bool_var = self.val > lhs.val 
         return bool_var ## BitVec(size=1, val = int(bool_val))
 
+    #greater than or equal to method
     def __ge__(self, lhs):
         bool_var = self.val >= lhs.val
         return bool_var ##BitVec(size=1, val = int(bool_val))
+
+    def __matmul__(self, lhs):
+        '''
+        Use @ operator for repetition and concatenation
+        If one of the argument is integer then it acts like repetition operator
+        If both arguments are bitvector, then it concatenates them
+        ex: a:BitVec and b:BitVec
+        2@a #Repeat a or concatenate a with itself
+        a@b #concat a with b
+
+        '''
+        if (type(lhs)==int):
+            v = self.val
+            for i in range(lhs):
+                v = (v<<self.size) | self.val
+            return BitVec(self.size*lhs, v)
+        elif (type(lhs)==BitVec):
+            rsize = self.size + lhs.size
+            outval = (self.val << lhs.size) | lhs.val
+            return BitVec(rsize, outval)
+
+    def __rmatmul__(self, rhs):
+        return self@rhs;
 
 
     #TODO: Complete the implementation of set
@@ -186,3 +272,6 @@ class BitVec:
     #    c[...] = 0x10 #Option 1
     #    c.v    = 0x10 #add an attr v and write setattr for that
     #    c[_]   = 0x10 #similar to first option; but declare a variable _ in the library
+
+
+
